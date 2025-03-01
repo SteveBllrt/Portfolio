eeebcd8d8425bf89bcffd33df76dcbbf9ad19cb6
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2025-03-01 14:29:12 +0800

    feat: added web hero new design

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,72 +5,80 @@ import { motion } from "motion/react";
 
 const Header = () => {
   return (
-    <div className="w-11/12 max-w-3xl h-screen flex items-center justify-center gap-4 mx-auto">
-      <div className="flex flex-col gap-2">
+    <div className="max-w-2xl h-screen flex flex-col-reverse md:flex-row items-center gap-4 justify-between md:mx-[160px]">
+      <div className="flex flex-col gap-4 justify-center">
         <motion.h3
           initial={{ y: -20, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.3, delay: 0.3 }}
+          transition={{ duration: 0.3, delay: 0.2 }}
           viewport={{ once: true }}
-          className="flex items-center gap-2 text-xl md:text-[72px] mb-4 font-Ovo"
+          className="flex items-center gap-2 md:text-[20px] 2xl:text-[22px] font-Ovo font-medium uppercase"
         >
-          Hi! I’m Steven Ballaret
-          <Image src={assets.hand} alt="Hand waving" className="w-10" />
+          Hello I am Steven Ballaret
+          <Image src={assets.hand} alt="Hand waving" className="w-5" />
         </motion.h3>
 
         <motion.p
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.3, delay: 0.7 }}
+          transition={{ duration: 0.3, delay: 0.4 }}
           viewport={{ once: true }}
-          className="max-w-3xl mx-auto font-Ovo mt-4"
+          className="font-Ovo mt-5 text-[70px]  2xl:text-[75px] text-[#15546b] dark:text-white"
         >
-          I am a frontend developer from Mandurriao, Iloilo, with years of
-          experience in various technologies, including React, Next.js, and
-          React Native.
+          Software Developer
         </motion.p>
 
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6 mr-2">
+        <motion.p
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.5 }}
+          viewport={{ once: true }}
+          className="font-Ovo mt-5 md:text-[20px] 2xl:text-[22px] md:max-w-[600px] 2xl:max-w-[800px] text-[#242424] dark:text-white"
+        >
+          I’m from Mandurriao, Iloilo, with years of experience working with
+          multiple technologies, including React, Next.js, and React Native
+        </motion.p>
+
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-5 mr-2">
           <motion.a
             initial={{ y: 23, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.3, delay: 1 }}
+            transition={{ duration: 0.3, delay: 0.6 }}
             viewport={{ once: true }}
             href="#contact"
-            className="md:px-10 px-8 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
+            className="md:px-8 px-8 py-4 border border-white rounded-3xl bg-[#484848] hover:bg-[#646464] text-white flex items-center gap-2 dark:bg-transparent"
           >
-            <p className="text-sm md:text-base"> contact me </p>
+            <p className="text-sm md:text-base"> connect with me </p>
             <Image src={assets.right_arrow_up} alt="" className="w-4" />
           </motion.a>
 
           <motion.a
             initial={{ y: 23, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.3, delay: 1.1 }}
+            transition={{ duration: 0.3, delay: 0.7 }}
             viewport={{ once: true }}
             href="./ballaret-resume.pdf"
             download
-            className="md:px-10 px-8 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
+            className="md:px-10 px-8 py-4 border rounded-3xl border-gray-500 hover:bg-[#F0F0F0] flex items-center gap-2 bg-white dark:text-black"
           >
             <p className="text-sm md:text-base"> my resume</p>
             <Image src={assets.download} alt="" className="w-4" />
           </motion.a>
         </div>
       </div>
-      <div className="flex items-center justify-center">
-        <motion.div
-          initial={{ scale: 0 }}
-          whileInView={{ scale: 1 }}
-          transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
-          animate={{ opacity: 1 }}
-          viewport={{ once: true }}
-        >
-          <Image
-            src={assets.hero_image}
-            alt="Profile"
-            className="rounded-full w-[740px] h-[740px]"
-          />
-        </motion.div>
+
+      <div className="">
+        <Image
+          src={assets.hero_image}
+          alt="Profile"
+          className="h-20 w-20 md:w-[471px] md:h-[666px] 2xl:w-[520px] 2xl:h-[740px] md:absolute md:bottom-0 md:right-10 z-10"
+        />
+
+        <Image
+          src={assets.circle_bg}
+          alt="Profile"
+          className="h-20 w-20 md:w-[620px] md:h-[600px] 2xl:h-[665px] 2xl:w-[690px] md:absolute md:bottom-0 md:right-0 z-0"
+        />
       </div>
     </div>
   );
